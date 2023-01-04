f64d5e4af29fc3056dfc37f81f6589ac4e289b01
xania <34893682+xania@users.noreply.github.com>
2023-01-04 19:16:34 +0100

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 /* time.tsx */
 import { jsx } from '@xania/view';
 import { State, combineLatest } from '@xania/state';
-import "./App.scss";
+import './App.scss';
 
 export function App() {
   const count = new State<number>();
-  const double = count.map((x) => {
-    console.log('Doubl', x);
-    return x * 2;
-  });
-  const triple = count.map((x) => {
-    console.log('Tripl', x);
-    return x * 3;
-  });
-  const quadruple = double.map((x) => {
-    console.log('Quadr', x);
-    return x * 2;
-  });
-  const sum = combineLatest([count, triple, quadruple]).map((values) => {
-    console.log('Sum', values);
-    return values.reduce((x, y) => x + y, 0);
-  });
+  const double = count.map((x) => x * 2);
+  const triple = count.map((x) => x * 3);
+  const quadruple = double.map((x) => x * 2);
+
+  const sum = combineLatest([count, triple, quadruple]).map((values) =>
+    values.reduce((x, y) => x + y, 0)
+  );
 
   return (
     <div class="App">
@@ -35,7 +26,7 @@ export function App() {
           </div>
         </div>
 
-        <button click={(_) => count.set((x) => (x | 0) + 1)}>increment</button>
+        <button click={(_) => count.set((x) => (x! | 0) + 1)}>increment</button>
       </header>
     </div>
   );
